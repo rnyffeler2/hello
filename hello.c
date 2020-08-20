acf86f37d1e7f997329b8ab630b5a7f1b025465f
rnyffeler2 <rnyffeler2@users.noreply.github.com>
2020-08-20 13:15:26 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Ryan Nyffeler
+ *         rnyffeler2@huskers.unl.edu
+ * Date: 2020/08/20
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Ryan Nyffeler\n");
+
+  return 0;
+}
